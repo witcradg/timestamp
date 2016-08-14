65c5198298c5360648e85127f81cac22208d7a5e
Dean <witcradg@gmail.com>
2016-08-14 21:10:42 +0000

    Change repository and implement using clementine.js

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
+<<<<<<< b2793115e64ee1c1bedc90ae5e1477922e410855
 var express = require('express');
-var app = express();
+=======
+'use strict';
 
+var express = require('express'),
+    routes = require('./app/routes/index.js');
+    
+>>>>>>> Change repository and implement using clementine.js
+var app = express();
+var router = express.Router();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/",router);
+routes(router);
+
+
+//Creating Router() object
+
+//var router = express.Router();
+
+// Provide all routes here, this is for Home page.
+
+
+
+// Tell express to use this router with /api before.
+// You can put just '/' if you don't want any sub path before routes.
+
+
+
+<<<<<<< b2793115e64ee1c1bedc90ae5e1477922e410855
 //TODO add Use statement when there is no / on the end of the url
 //TODO display error message and Use statement when an invalid value is submitted
 
@@ -19,4 +47,8 @@ app.get('/*', function (req, res) {
 
 app.listen(process.env.PORT || 8080, function () {
   console.log('app listening on port.');
+=======
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  console.log("server listening");
+>>>>>>> Change repository and implement using clementine.js
 });
